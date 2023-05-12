9cb009ac20e7c4112c6ade507e57268ab1d4bac7
Shahin Nezam <shahinnezam@hotmail.com>
2023-05-12 15:19:22 -0700

    Blank Grid Unit Test Green/Refactor - Edited incorrect code to properly run unit test to check for blank cells

--- TDD_TicTacToe/Model.swift
+++ TDD_TicTacToe/Model.swift
@@ -7,23 +7,23 @@
 
 import Foundation
 
-enum cell: String {
+enum Cell: String {
     case x = "X"
     case o = "O"
     case b = "" // blank
 }
 
 struct TicModel {
-    private var _grid: [cell]
+    private var _grid: [Cell]
     
     init() {
         _grid = []
         for _ in 0..<9 {
-            _grid.append(cell.x)
+            _grid.append(Cell.b)
         }
     }
     
-    var grid: [cell] {
+    var grid: [Cell] {
         get { _grid }
     }
 
