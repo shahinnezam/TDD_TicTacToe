4412e54f0401b575d2cc0617434c2c87b2b9cdf0
Shahin Nezam <shahinnezam@hotmail.com>
2023-05-12 12:14:17 -0700

    Model Unit Test Green - Added grid code to model for checking 9 cells

--- TDD_TicTacToe/Model.swift
+++ TDD_TicTacToe/Model.swift
@@ -7,6 +7,26 @@
 
 import Foundation
 
-struct TicModel {
-        
+enum cell: String {
+    case x = "X"
+    case o = "O"
+    case b = "" // blank
 }
+
+struct TicModel {
+    private var _grid: [cell]
+    
+    init() {
+        _grid = []
+        for _ in 0..<9 {
+            _grid.append(cell.x)
+        }
+    }
+    
+    var grid: [cell] {
+        get { _grid }
+    }
+
+}
+
+
